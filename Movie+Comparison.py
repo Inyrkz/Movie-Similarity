ed90eca601dd2282393291df4caf688e75075e45
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2019-10-03 17:30:05 +0100

    Add files via upload
    Created with Jupyter Notebook in .py format

--- Movie+Comparison.py
+++ Movie+Comparison.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[11]:
 
 
 #Importing Libraries
@@ -14,7 +14,9 @@ np.random.seed(5)
 
 
 #Read Wiki_plot and IMDb plot
-movies_wi = pd.read_csv('Desktop/movies.csv')
+movies_wi = pd.read_csv('Desktop/movies.csv', index_col = 0)
+#index_col=0 to remove the index column
+
 
 #Printing the number of movies loaded
 print("Total number of movies loaded: %s" % (len(movies_wi)))
@@ -23,7 +25,17 @@ print("Total number of movies loaded: %s" % (len(movies_wi)))
 movies_wi.head()
 
 
-# In[7]:
+# In[13]:
+
+
+#view the information of the data
+movies_wi.info()
+
+#view description of the data
+movies_wi.describe()
+
+
+# In[14]:
 
 
 #Let's combine the wiki_plot and the imdb_plot into one column called plot
@@ -33,24 +45,47 @@ movies_wi['plot'] = movies_wi['wiki_plot'].astype(str) + "\n" + movies_wi['imdb_
 movies_wi.head()
 
 
-# In[11]:
+# In[15]:
 
 
-# #tokenizing a paragraph into sentences and storing in token_sent
-# token_sent = [sent for sent in nltk.sent_tokenize("""Today (May 19, 2016) is the only daughter's wedding. Vito Corleone is the Godfather.""")]
+#create a new column that shows the number of words in each plot
+movies_wi["length"] = movies_wi["plot"].apply(len)
 
-# # tokenizing word in first sentence from token_sent, and saving as token_word
-# token_word = [word for word in nltk.word_tokenize(token_sent[0])]
+movies_wi.head()  # display the top part of the dataset
 
-# #Removing tokens that do not contain any letters from token_word
+
+# In[18]:
+
+
+#a histogram that shows the distribution of the number of words
+movies_wi["length"].plot.hist(bins = 50)
+
+
+# In[19]:
+
+
+#Description of the length column
+movies_wi["length"].describe()
+
+
+# In[22]:
+
+
+#tokenizing a paragraph into sentences and storing in token_sent
+token_sent = [sent for sent in nltk.sent_tokenize("""Today (May 19, 2016) is the only daughter's wedding. Vito Corleone is the Godfather.""")]
+
+# tokenizing word in first sentence from token_sent, and saving as token_word
+token_word = [word for word in nltk.word_tokenize(token_sent[0])]
+
+#Removing tokens that do not contain any letters from token_word
 import re #importing re; regular expression
-# refined = [word for word in token_word if re.search('[a-zA-Z]', word)]
+refined = [word for word in token_word if re.search('[a-zA-Z]', word)]
 
-# #View refined words to observe words after tokenization
-# print(refined)
+#View refined words to observe words after tokenization
+print(refined)
 
 
-# In[12]:
+# In[23]:
 
 
 #Importing the SnowballStemmer to perform stemming
@@ -70,12 +105,12 @@ def tokenize_stem(your_text):
     
     return stem_refined
 
-#Now let's invoke our tokenize_stem function
-example = tokenize_stem("Today (May 19, 2016) is his only daughter's wedding.")
-print(example)
+
+#applying the function to the first five rows of the plot column to see
+movies_wi['plot'].head(5).apply(tokenize_stem)
 
 
-# In[13]:
+# In[24]:
 
 
 #Import TfidfVectorizer to create TF-IDF vectors 
@@ -84,7 +119,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000, min_df=0.2, stop_words='english', use_idf=True, tokenizer=tokenize_stem, ngram_range=(1,3))
 
 
-# In[14]:
+# In[25]:
 
 
 #Applying fit and transform of tfidf_vectorizer with the plot of each movie
@@ -94,7 +129,7 @@ tfidf_matrix = tfidf_vectorizer.fit_transform([x for x in movies_wi["plot"]])
 print(tfidf_matrix.shape)
 
 
-# In[17]:
+# In[26]:
 
 
 # To perform clustering, we import k-means
@@ -115,7 +150,7 @@ movies_wi["cluster"] = our_cluster
 movies_wi["cluster"].value_counts()
 
 
-# In[18]:
+# In[27]:
 
 
 #Importing cosine_similarity to calculate how similar the movie plots are
@@ -126,7 +161,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 sim_dist = 1 - cosine_similarity(tfidf_matrix)
 
 
-# In[20]:
+# In[28]:
 
 
 #Importing matplotlib.pyplot to plot graphs
@@ -151,4 +186,11 @@ fig.set_size_inches(80, 80)
 
 #Now let's view the plotted dendrogram
 plt.show()
+
+
+# In[31]:
+
+
+#Which movies are most similar?
+print('A place in the sun', 'and', "It's a Wonderful life", 'are the most similar movies')
 
